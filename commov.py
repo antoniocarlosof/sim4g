fa6e718681a9c52eb198173a151b8458def81cca
Leonardo <lcfossi@gmail.com>
2020-12-07 21:03:58 -0300

    Raios ponderados pelas probabilidades de indoor, outdoor, incar. Entradas por prompt

--- commov.py
+++ commov.py
@@ -34,11 +34,10 @@ def count_hex(area, radius):
 
     return hex_area, total_hex
 
-def link_budget(pot_tx, pot_rx, Ms, Gt, Pt):
-    Lmax = pot_tx - pot_rx + Gt - Pt - Ms
+def link_budget(pot_tx, pot_rx, Ms, Gt, Pt, decrease):
+    Lmax = pot_tx - pot_rx + Gt - Pt - Ms - decrease
     return Lmax
 
-#Raio máximo é determinado a partir do modelo de Okumura-Hata, resolvido para a distância ao utilizar-se a maior perda aceitável (Link Budget)
 def max_radius(max_loss, freq):
     alfa = 4
     beta = 10.2
@@ -59,39 +58,12 @@ def loss_model(dist, freq):
 
     return loss
 
-
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Input arguments")
-    parser.add_argument('--bandwidth', '-b',
-                        type=float,
-                        help="Channel bandwidth to work on. It must be 1.4, 3, 5, 10, 15 or 20 [MHz]. Default = 20.",
-                        default=20)
-    parser.add_argument('--area', '-a',
-                        type=float,
-                        help="Area to cover with LTE [km]. Required argument, without default value.",
-                        required=True)
-    parser.add_argument('--throughput', '-t',
-                        type=float,
-                        help="Mean throughput desired for the LTE network [Mbps]. Default = 25.",
-                        default=25)
-
-    inputs = parser.parse_args()
-
-    bw = inputs.bandwidth
-    area = inputs.area
-    desired_throughput = inputs.throughput
-    ro = inputs.ro
-    gama = inputs.gama
-    eta = inputs.eta
-
-    n = 4
-    sigma = 7.6
-    prob_cobertura_celula = 0.9
+def outdoor_radius(sigma, n, ro, gama, eta, bw, figura)
 
     Ms = 4*n/sigma - 3
     Q = (erfc(Ms/(sigma*sqrt(2)))/2
     prob_cobertura_borda = 1 - Q
-    
+
     sigma_sir = sqrt(2*sigma*(1-ro))
     Q_inv = sqrt(2)*erfcinv(2*prob_cobertura_borda)
     M_in = -1*Q_inv*sigma_sir
@@ -118,17 +90,155 @@ if __name__ == "__main__":
                 break
             else:
                 break
-        
+
         mcs[modulation].append(temp_rate)
         mcs[modulation].append(temp_sinr)
-        
+
         sens = temp_sinr + figura + 10*log10(180000) - 174 + D_in
-        loss = link_budget(P_tx, 
-                            sens, 
+        loss = link_budget(P_tx,
+                            sens,
                             Ms,
                             uplink_multipath + uplink_Gtx + uplink_Grx,
                             uplink_rx_loss + uplink_tx_loss)
-        radius = max_radius(loss, 2000)
+        return max_radius(loss, 2000)
+
+def indoor_radius(sigma, n, ro, gama, eta, bw, figura)
+    sigma = sqrt(64 + pow(sigma, 2))
+
+    Ms = 4*n/sigma - 3
+    Q = (erfc(Ms/(sigma*sqrt(2)))/2
+    prob_cobertura_borda = 1 - Q
+
+    sigma_sir = sqrt(2*sigma*(1-ro))
+    Q_inv = sqrt(2)*erfcinv(2*prob_cobertura_borda)
+    M_in = -1*Q_inv*sigma_sir
+
+    m_in = pow(10, M_in/10)
+    D_in = 10*log10(m_in*gama*eta)
+
+    rate_rb = float()
+    rate_total = float()
+    temp_rate = float()
+    temp_sinr = float()
+
+    for modulation in mcs:
+        for sinr in reversed(range(0, 34)):
+            rate_rb = mcs[modulation][2]/(mcs[modulation][3] + pow(e, mcs[modulation][4]*sinr))
+            rate_total = rate_rb*(5*bw)
+
+            if rate_total > desired_throughput:
+                temp = rate_total
+                temp_sinr = sinr
+            elif rate_total == desired_throughput:
+                temp = rate_total
+                temp_sinr = sinr
+                break
+            else:
+                break
+
+        mcs[modulation].append(temp_rate)
+        mcs[modulation].append(temp_sinr)
+
+        sens = temp_sinr + figura + 10*log10(180000) - 174 + D_in
+        loss = link_budget(P_tx,
+                            sens,
+                            Ms,
+                            uplink_multipath + uplink_Gtx + uplink_Grx,
+                            uplink_rx_loss + uplink_tx_loss)
+        return max_radius(loss, 2000)
+
+def incar_radius(sigma, n, ro, gama, eta, bw, figura)
+    sigma = sqrt(36 + pow(sigma, 2))
+
+    Ms = 4*n/sigma - 3
+    Q = (erfc(Ms/(sigma*sqrt(2)))/2
+    prob_cobertura_borda = 1 - Q
+
+    sigma_sir = sqrt(2*sigma*(1-ro))
+    Q_inv = sqrt(2)*erfcinv(2*prob_cobertura_borda)
+    M_in = -1*Q_inv*sigma_sir
+
+    m_in = pow(10, M_in/10)
+    D_in = 10*log10(m_in*gama*eta)
+
+    rate_rb = float()
+    rate_total = float()
+    temp_rate = float()
+    temp_sinr = float()
+
+    for modulation in mcs:
+        for sinr in reversed(range(0, 34)):
+            rate_rb = mcs[modulation][2]/(mcs[modulation][3] + pow(e, mcs[modulation][4]*sinr))
+            rate_total = rate_rb*(5*bw)
+
+            if rate_total > desired_throughput:
+                temp = rate_total
+                temp_sinr = sinr
+            elif rate_total == desired_throughput:
+                temp = rate_total
+                temp_sinr = sinr
+                break
+            else:
+                break
+
+        mcs[modulation].append(temp_rate)
+        mcs[modulation].append(temp_sinr)
+
+        sens = temp_sinr + figura + 10*log10(180000) - 174 + D_in
+        loss = link_budget(P_tx,
+                            sens,
+                            Ms,
+                            uplink_multipath + uplink_Gtx + uplink_Grx,
+                            uplink_rx_loss + uplink_tx_loss)
+        return max_radius(loss, 2000)
+
+if __name__ == "__main__":
+
+    inputs = {
+        "bandwidth" = 0,
+        "area" = 0,
+        "throughput" = 0,
+        "ro" = 0,
+        "gama" = 0,
+        "eta" = 0,
+        "prob_outdoor" = 0,
+        "prob_indoor" = 0,
+        "prob_incar" = 0,
+        "figura" = 0
+    }
+
+    #inputs
+    inputs.bandwidth = input("Please select Bandwidth: ")
+    inputs.area = input("Select desired area to be covered: ")
+    inputs.throughput = input("Select desired throughput: ")
+    inputs.ro = input("Select ro: ")
+    inputs.gama = input("Select gama: ")
+    inputs.eta = input("Select eta: ")
+    inputs.prob_outdoor = input("Select probability of user being outdoors: ")
+    inputs.prob_indoor = input("Select probability of user being indoors: ")
+    inputs.prob_incar = input("Select probability of user being in a car: ")
+
+
+    bw = inputs.bandwidth
+    area = inputs.area
+    desired_throughput = inputs.throughput
+    ro = inputs.ro
+    gama = inputs.gama
+    eta = inputs.eta
+    prob_outdoor = inputs.prob_outdoor
+    prob_indoor = inputs.prob_indoor
+    prob_incar = inputs.prob_incar
+
+
+    n = 4
+    sigma = 7.6
+    prob_cobertura_celula = 0.9
+    
+    #substituir as entradas ->
+    out_r = outdoor_radius(sigma, n, ro, gama, eta, bw, figura)
+    indoor_r = indoor_radius(sigma, n, ro, gama, eta, bw, figura)
+    incar_r = incar_radius(sigma, n, ro, gama, eta, bw, figura)
+
+    radius = prob_outdoor*out_r + prob_indoor*indoor_r + prob_incar*incar_r
 
     mcs = start_mcs()
-    
